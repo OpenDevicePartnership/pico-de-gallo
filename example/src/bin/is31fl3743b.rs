517fe65a87fa394b156e34361c1eb2b891477340
Felipe Balbi <febalbi@microsoft.com>
2025-08-28 12:12:05 -0700

    Use new lumisil driver methods
    By controlling all LEDs in one SPI message, the effect looks much
    better.
    
    Signed-off-by: Felipe Balbi <febalbi@microsoft.com>

--- example/src/bin/is31fl3743b.rs
+++ example/src/bin/is31fl3743b.rs
@@ -4,40 +4,6 @@ use is31fl3743b_driver::{CSy, Is31fl3743b, SWx};
 use pico_de_gallo::PicoDeGallo;
 use std::time::Duration;
 
-const COLS: [SWx; 11] = [
-    SWx::SW1,
-    SWx::SW2,
-    SWx::SW3,
-    SWx::SW4,
-    SWx::SW5,
-    SWx::SW6,
-    SWx::SW7,
-    SWx::SW8,
-    SWx::SW9,
-    SWx::SW10,
-    SWx::SW11,
-];
-const ROWS: [CSy; 18] = [
-    CSy::CS1,
-    CSy::CS2,
-    CSy::CS3,
-    CSy::CS4,
-    CSy::CS5,
-    CSy::CS6,
-    CSy::CS7,
-    CSy::CS8,
-    CSy::CS9,
-    CSy::CS10,
-    CSy::CS11,
-    CSy::CS12,
-    CSy::CS13,
-    CSy::CS14,
-    CSy::CS15,
-    CSy::CS16,
-    CSy::CS17,
-    CSy::CS18,
-];
-
 fn main() -> Result<()> {
     color_eyre::install()?;
 
@@ -57,18 +23,13 @@ fn main() -> Result<()> {
     // Set global current, check method documentation for more info
     let _ = driver.set_global_current(90);
 
+    let _ = driver.set_led_peak_current_bulk(SWx::SW1, CSy::CS1, &vec![100; 11 * 18]);
+
     // Driver is fully set up, we can now start turning on LEDs!
     // Create a white breathing effect
     loop {
-        for brightness in (0..=80).step_by(10).chain((0..=80).step_by(10).rev()) {
-            for i in ROWS.into_iter() {
-                for j in COLS.into_iter() {
-                    // Set scaling register to max current
-                    let _ = driver.set_led_peak_current(j, i, 100);
-                    // Set PWM brightness register
-                    let _ = driver.set_led_brightness(j, i, brightness);
-                }
-            }
+        for brightness in (0..=255_u8).chain((0..=255).rev()) {
+            let _ = driver.set_led_brightness_bulk(SWx::SW1, CSy::CS1, &vec![brightness; 11 * 18]);
             std::thread::sleep(Duration::from_micros(1));
         }
     }
