720803527149ef0e615883ef211c5d7fd1f9182c
Felipe Balbi <febalbi@microsoft.com>
2025-08-28 14:40:49 -0700

    Add mpu-6050 example
    Signed-off-by: Felipe Balbi <febalbi@microsoft.com>

---
+++ example/src/bin/mpu6050.rs
@@ -0,0 +1,58 @@
+use color_eyre::Result;
+use mpu6050_dmp::address::Address;
+use mpu6050_dmp::sensor::Mpu6050;
+use pico_de_gallo::PicoDeGallo;
+use std::time::Duration;
+
+fn main() -> Result<()> {
+    color_eyre::install()?;
+
+    let gallo = PicoDeGallo::new(Default::default())?;
+    let i2c = gallo.clone();
+    let mut delay = gallo.clone();
+
+    let mut sensor = Mpu6050::new(i2c, Address::default()).unwrap();
+
+    println!("Initializing sensor");
+    sensor.initialize_dmp(&mut delay).unwrap();
+    println!("Initialization complete");
+
+    let accel_data = sensor.accel().unwrap();
+    println!(
+        "Accelerometer [mg]: x={}, y={}, z={}",
+        accel_data.x() as i32,
+        accel_data.y() as i32,
+        accel_data.z() as i32
+    );
+
+    let gyro_data = sensor.gyro().unwrap();
+    println!(
+        "Gyroscope [deg/s]: x={}, y={}, z={}",
+        gyro_data.x() as i32,
+        gyro_data.y() as i32,
+        gyro_data.z() as i32
+    );
+
+    loop {
+        let (accel, gyro, temp) = (
+            sensor.accel().unwrap(),
+            sensor.gyro().unwrap(),
+            sensor.temperature().unwrap().celsius(),
+        );
+        println!("Sensor Readings:");
+        println!(
+            "  Accelerometer [mg]: x={}, y={}, z={}",
+            accel.x() as i32,
+            accel.y() as i32,
+            accel.z() as i32
+        );
+        println!(
+            "  Gyroscope [deg/s]: x={}, y={}, z={}",
+            gyro.x() as i32,
+            gyro.y() as i32,
+            gyro.z() as i32
+        );
+        println!("  Temperature: {:.2}C", temp);
+        std::thread::sleep(Duration::from_secs(1));
+    }
+}
