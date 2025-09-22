5e685258276bfa6080005f46cbe27fd4524f1886
Felipe Balbi <febalbi@microsoft.com>
2025-09-22 16:44:14 -0700

    app: pretty print i2c scan table
    Signed-off-by: Felipe Balbi <febalbi@microsoft.com>

--- crates/pico-de-gallo-app/src/lib.rs
+++ crates/pico-de-gallo-app/src/lib.rs
@@ -2,6 +2,9 @@ use clap::{Parser, Subcommand};
 use color_eyre::{Result, eyre::eyre};
 use pico_de_gallo_lib::PicoDeGallo;
 use std::num::ParseIntError;
+use tabled::builder::Builder;
+use tabled::settings::object::Rows;
+use tabled::settings::{Alignment, Style};
 
 #[derive(Parser, Debug)]
 #[command(
@@ -24,14 +27,14 @@ enum Commands {
     /// Get firmware version
     Version,
 
-    /// I2C accessor
+    /// I2C access methods
     I2c {
         /// I2C commands
         #[command(subcommand)]
         command: Option<I2cCommands>,
     },
 
-    /// SPI accessor
+    /// SPI access methods
     Spi {
         /// SPI commands
         #[command(subcommand)]
@@ -41,19 +44,19 @@ enum Commands {
     /// Set bus parameters for I2C and SPI
     SetConfig {
         /// I2C frequency
-        #[arg(short = 'i', long)]
+        #[arg(long)]
         i2c_frequency: u32,
 
         /// SPI frequency
-        #[arg(short = 's', long)]
+        #[arg(long)]
         spi_frequency: u32,
 
         /// SPI phase first transition
-        #[arg(short = 'p', long, default_value_t)]
+        #[arg(long, default_value_t)]
         spi_first_transition: bool,
 
         /// SPI polarity idle low
-        #[arg(short = 'o', long, default_value_t)]
+        #[arg(long, default_value_t)]
         spi_idle_low: bool,
     },
 }
@@ -67,7 +70,7 @@ enum I2cCommands {
         reserved: bool,
     },
 
-    /// Read
+    /// Read bytes through the I2C bus from device at given address
     Read {
         /// I2C slave address
         #[arg(short, long, value_parser(parse_byte))]
@@ -78,7 +81,7 @@ enum I2cCommands {
         count: usize,
     },
 
-    /// Write
+    /// Write bytes through I2C bus to device at given address
     Write {
         /// I2C slave address
         #[arg(short, long, value_parser(parse_byte))]
@@ -89,7 +92,7 @@ enum I2cCommands {
         bytes: Vec<u8>,
     },
 
-    /// Write then read
+    /// Write bytes follwed by read bytes
     WriteRead {
         /// I2C slave address
         #[arg(short, long, value_parser(parse_byte))]
@@ -107,21 +110,21 @@ enum I2cCommands {
 
 #[derive(Subcommand, Debug)]
 enum SpiCommands {
-    /// Read
+    /// Read bytes through SPI bus
     Read {
         /// Number of bytes to read
         #[arg(short, long)]
         count: usize,
     },
 
-    /// Write
+    /// Write bytes through SPI bus
     Write {
         /// Bytes to transfer
         #[arg(short, long, num_args(1..), value_parser(parse_byte))]
         bytes: Vec<u8>,
     },
 
-    /// Write followed by read.
+    /// Write bytes followed by read bytes
     WriteRead {
         /// Number of bytes to read
         #[arg(short, long)]
@@ -191,50 +194,46 @@ impl Cli {
             PicoDeGallo::new()
         };
 
-        let mut high = 0;
-        print!(
-            r#"
-   0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f
-{:x} "#,
-            high
+        let mut builder = Builder::with_capacity(17, 8);
+        builder.push_record(
+            (0..=16)
+                .map(|i| if i == 0 { String::new() } else { format!("{:x}", i - 1) })
+                .collect::<Vec<_>>(),
         );
 
-        for address in 0..=0x7f_u8 {
-            match address {
-                0x00..=0x07 | 0x78..=0x7f => {
-                    if reserved {
-                        let result = pg.i2c_read(address, 1).await;
+        for hi in 0..=7 {
+            let mut row = vec![format!("{:x} ", hi)];
 
-                        if result.is_ok() {
-                            print!("{:02x} ", address);
+            for lo in 0..=15 {
+                let address = hi << 4 | lo;
+                let stat = match address {
+                    0x00..=0x07 | 0x78..=0x7f => {
+                        if reserved {
+                            match pg.i2c_read(address, 1).await {
+                                Ok(_) => format!("{:02x}", address),
+                                Err(_) => format!("--"),
+                            }
                         } else {
-                            print!("-- ");
+                            format!("RR")
                         }
-                    } else {
-                        print!("RR ");
                     }
-                }
+                    _ => match pg.i2c_read(address, 1).await {
+                        Ok(_) => format!("{:02x}", address),
+                        Err(_) => format!("--"),
+                    },
+                };
 
-                _ => {
-                    let result = pg.i2c_read(address, 1).await;
-                    if result.is_ok() {
-                        print!("{:02x} ", address);
-                    } else {
-                        print!("-- ");
-                    }
-                }
+                row.push(stat);
             }
 
-            if address & 0x0f == 0x0f {
-                high += 1;
-                print!("\n");
-
-                if high < 8 {
-                    print!("{:x} ", high);
-                }
-            }
+            builder.push_record(row);
         }
-        print!("\n");
+
+        let mut table = builder.build();
+        table.modify(Rows::first(), Alignment::right());
+        table.with(Style::rounded());
+
+        println!("{}", table);
 
         Ok(())
     }
