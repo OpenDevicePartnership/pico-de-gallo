ed79524a48ac0e659781b3d7cccb1eba136cbaf5
Felipe Balbi <febalbi@microsoft.com>
2025-10-07 15:06:47 -0700

    ffi: Generate header to OUT_DIR
    Signed-off-by: Felipe Balbi <febalbi@microsoft.com>

--- crates/pico-de-gallo-ffi/build.rs
+++ crates/pico-de-gallo-ffi/build.rs
@@ -3,9 +3,8 @@ fn main() {
     use std::path::PathBuf;
 
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-    let header_path = PathBuf::from(&crate_dir)
-        .join("include")
-        .join("pico_de_gallo.h");
+    let out = env::var("OUT_DIR").unwrap();
+    let header_path = PathBuf::from(&out).join("include").join("pico_de_gallo.h");
 
     cbindgen::generate(crate_dir)
         .expect("Unable to generate bindings")
